a23f82d44447b412437b069bd937e0e4f7e57c21
Nanda <117591047+Al4nDi3go@users.noreply.github.com>
2023-08-01 14:43:06 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+echo "olá cuméonom";
+$turma2 = "Vamos desenvolver website em PHP";
+
+//o comando echo transmite a mensagem na tela
+
+echo "<br>" .$turma2;
+
+$x = 50 + 76;
+echo "<br>".$x;
+
+$txt = "Olá imundos";
+$x = 5;
+$y = 10.5;
+echo "<br>".$txt." x = ".$x." y = ".$y
+?>
+
+</body>
+</html>
